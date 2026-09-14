f64abc6943e127700f0d51ec4319c55bf63501d8
272499 <272499@unifio.edu.br>
2026-09-14 23:54:57 +0000

    1. Apresentação do usuário

--- primeiro/src/app/app-module.ts
+++ primeiro/src/app/app-module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ListaExercicios1Module } from './lista-exercicios-1/lista-exercicios-1-module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ListaExercicios1Module
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
---
+++ primeiro/src/app/lista-exercicios-1/exercicio-01/exercicio-01.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio-01',
+  standalone: false,
+  templateUrl: './exercicio-01.html',
+  styleUrl: './exercicio-01.scss',
+})
+export class Exercicio01 {
+  nome = 'Carlos';
+  idade = 25;
+  curso = 'Sistemas de Informação';
+  
+}
+
