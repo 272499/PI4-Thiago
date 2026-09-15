90d5360a822f707c0819e190d79a3dbba457dcab
272499 <272499@unifio.edu.br>
2026-09-15 22:37:52 +0000

    7. Nome em tempo real

---
+++ primeiro/src/app/lista-exercicios-1/exercicio-07/exercicio-07.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio-07',
+  standalone: false,
+  templateUrl: './exercicio-07.html',
+  styleUrl: './exercicio-07.scss',
+})
+export class Exercicio07 {
+  nome = 'Thiago';
+}
--- primeiro/src/app/lista-exercicios-1/lista-exercicios-1-module.ts
+++ primeiro/src/app/lista-exercicios-1/lista-exercicios-1-module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import { Exercicio01 } from './exercicio-01/exercicio-01';
 import { Exercicio02 } from './exercicio-02/exercicio-02';
 import { Exercicio03 } from './exercicio-03/exercicio-03';
 import { Exercicio04 } from './exercicio-04/exercicio-04';
 import { Exercicio05 } from './exercicio-05/exercicio-05';
 import { Exercicio06 } from './exercicio-06/exercicio-06';
+import { Exercicio07 } from './exercicio-07/exercicio-07';
 
 @NgModule({
-  declarations: [Exercicio01, Exercicio02, Exercicio03, Exercicio04, Exercicio05, Exercicio06],
-  imports: [CommonModule, FormsModule],
-  exports: [Exercicio01, Exercicio02, Exercicio03, Exercicio04, Exercicio05, Exercicio06],
+  declarations: [
+    Exercicio01,
+    Exercicio02,
+    Exercicio03,
+    Exercicio04,
+    Exercicio05,
+    Exercicio06,
+    Exercicio07
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    Exercicio01,
+    Exercicio02,
+    Exercicio03,
+    Exercicio04,
+    Exercicio05,
+    Exercicio06,
+    Exercicio07
+  ],
 })
 export class ListaExercicios1Module {}
