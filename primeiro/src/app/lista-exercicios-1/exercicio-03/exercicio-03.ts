e93ed17d01a899b40b30875dc7d1482642257f56
272499 <272499@unifio.edu.br>
2026-09-15 00:52:19 +0000

    3. Imagem dinâmica

---
+++ primeiro/src/app/lista-exercicios-1/exercicio-03/exercicio-03.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio-03',
+  standalone: false,
+  templateUrl: './exercicio-03.html',
+  styleUrl: './exercicio-03.scss',
+})
+export class Exercicio03 {
+  imagemProduto = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbVBBsu41CyPpbfe-GWo21z6Gs5u2oGmF7-xs9Jl3qre4E6_Yi8My2aVHu&s=10';
+  descricaoImagem = 'Meu Pai';
+  
+}
--- primeiro/src/app/lista-exercicios-1/lista-exercicios-1-module.ts
+++ primeiro/src/app/lista-exercicios-1/lista-exercicios-1-module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Exercicio01 } from './exercicio-01/exercicio-01';
 import { Exercicio02 } from './exercicio-02/exercicio-02';
+import { Exercicio03 } from './exercicio-03/exercicio-03';
 
 @NgModule({
-  declarations: [Exercicio01, Exercicio02],
+  declarations: [Exercicio01, Exercicio02, Exercicio03],
   imports: [CommonModule, FormsModule],
-  exports: [Exercicio01, Exercicio02],
+  exports: [Exercicio01, Exercicio02, Exercicio03],
 })
 export class ListaExercicios1Module {}
