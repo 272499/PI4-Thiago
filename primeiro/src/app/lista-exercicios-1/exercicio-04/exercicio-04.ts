d639206277dbc324a376a79cddd04ff757ebda69
272499 <272499@unifio.edu.br>
2026-09-15 01:01:36 +0000

    4. Botão habilitado ou desabilitado

---
+++ primeiro/src/app/lista-exercicios-1/exercicio-04/exercicio-04.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio-04',
+  standalone: false,
+  templateUrl: './exercicio-04.html',
+  styleUrl: './exercicio-04.scss',
+})
+export class Exercicio04 {
+  formularioValido = false;
+
+}
