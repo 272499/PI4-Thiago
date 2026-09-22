ea0dbac4b5ada6554db09fc23f080decf9b8f272
272499 <272499@unifio.edu.br>
2026-09-22 00:36:41 +0000

    7. Cores alternadas

---
+++ primeiro/src/app/lista-exercicios-2/exercicio-07/exercicio-07.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista2-exercicio-07',
+  standalone: false,
+  templateUrl: './exercicio-07.html',
+  styleUrl: './exercicio-07.scss',
+})
+export class Exercicio07 {
+  disciplinas = [
+    'Arquitetura e Administração de Banco de Dados',
+    'Padrões de Desenvolvimento Web',
+    'Códigos de Alta Performance',
+    'Projeto Integrador',
+    'Fundamentos de Redes e Segurança',
+    'História'
+  ];
+}
--- primeiro/src/app/lista-exercicios-2/lista-exercicios-2-module.ts
+++ primeiro/src/app/lista-exercicios-2/lista-exercicios-2-module.ts
@@ -7,10 +7,27 @@ import { Exercicio03 } from './exercicio-03/exercicio-03';
 import { Exercicio04 } from './exercicio-04/exercicio-04';
 import { Exercicio05 } from './exercicio-05/exercicio-05';
 import { Exercicio06 } from './exercicio-06/exercicio-06';
+import { Exercicio07 } from './exercicio-07/exercicio-07';
 
 @NgModule({
-  declarations: [Exercicio01, Exercicio02, Exercicio03, Exercicio04, Exercicio05, Exercicio06],
+  declarations: [
+    Exercicio01,
+    Exercicio02,
+    Exercicio03,
+    Exercicio04,
+    Exercicio05,
+    Exercicio06,
+    Exercicio07,
+  ],
   imports: [CommonModule, FormsModule],
-  exports: [Exercicio01, Exercicio02, Exercicio03, Exercicio04, Exercicio05, Exercicio06],
+  exports: [
+    Exercicio01,
+    Exercicio02,
+    Exercicio03,
+    Exercicio04,
+    Exercicio05,
+    Exercicio06,
+    Exercicio07,
+  ],
 })
 export class ListaExercicios2Module {}
