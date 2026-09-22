3f72ae84d4005f640bb69b042036566359fd691a
272499 <272499@unifio.edu.br>
2026-09-22 00:23:58 +0000

    5. Lista de nomes

---
+++ primeiro/src/app/lista-exercicios-2/exercicio-05/exercicio-05.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista2-exercicio-05',
+  standalone: false,
+  templateUrl: './exercicio-05.html',
+  styleUrl: './exercicio-05.scss',
+})
+export class Exercicio05 {
+  nomes = [
+    'Jonas',
+    'Marcos',
+    'Lucas',
+    'João',
+    'Pedro'
+  ];
+}
--- primeiro/src/app/lista-exercicios-2/lista-exercicios-2-module.ts
+++ primeiro/src/app/lista-exercicios-2/lista-exercicios-2-module.ts
@@ -5,10 +5,11 @@ import { Exercicio01 } from './exercicio-01/exercicio-01';
 import { Exercicio02 } from './exercicio-02/exercicio-02';
 import { Exercicio03 } from './exercicio-03/exercicio-03';
 import { Exercicio04 } from './exercicio-04/exercicio-04';
+import { Exercicio05 } from './exercicio-05/exercicio-05';
 
 @NgModule({
-  declarations: [Exercicio01, Exercicio02, Exercicio03, Exercicio04],
+  declarations: [Exercicio01, Exercicio02, Exercicio03, Exercicio04, Exercicio05],
   imports: [CommonModule, FormsModule],
-  exports: [Exercicio01, Exercicio02, Exercicio03, Exercicio04],
+  exports: [Exercicio01, Exercicio02, Exercicio03, Exercicio04, Exercicio05],
 })
 export class ListaExercicios2Module {}
